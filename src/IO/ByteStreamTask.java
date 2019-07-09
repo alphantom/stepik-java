53d638c059182ddc4ec3442faa092185f97c2be4
Albina Bystritskaya <al.Phant@yandex.ru>
2019-07-09 11:26:08 +0300

    Solved task 5.2.1 (stream's checksum)

---
+++ src/IO/ByteStreamTask.java
@@ -0,0 +1,38 @@
+package IO;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ByteStreamTask {
+
+    public static void main(String... args) {
+
+        try(InputStream inputStream = new ByteArrayInputStream(new byte[] {0x33, 0x45, 0x01})) {
+            System.out.println(checkSumOfStream(inputStream));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Контрольная сумма представляет собой число типа int
+     * Контрольная сумма пустого набора данных равна нулю.
+     * Контрольная сумма непустого набора данных вычисляется по следующей рекуррентной формуле:
+     *  Cn+1=rotateLeft(Cn) xor bn+1
+     *      Cn — контрольная сумма первых n байт данных,
+     *      rotateLeft — циклический сдвиг бит числа на один бит влево,
+     *      bn — n-ный байт данных.
+     * @param inputStream набор данных
+     * @return контрольная сумма
+     * @throws IOException
+     */
+    public static int checkSumOfStream(InputStream inputStream) throws IOException {
+        int checkSum = 0;
+        int nextByte;
+        while ((nextByte = inputStream.read()) != -1) {
+            checkSum = Integer.rotateLeft(checkSum, 1)^nextByte;
+        }
+        return checkSum;
+    }
+}
