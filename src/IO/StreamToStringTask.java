dd4fee9fda56a7c5c519bc7f667aa21ca6e41cab
Albina Bystritskaya <al.Phant@yandex.ru>
2019-07-09 16:09:51 +0300

    Solved task 5.3.1 (convert inputstream to string with charset)

---
+++ src/IO/StreamToStringTask.java
@@ -0,0 +1,21 @@
+package IO;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+public class StreamToStringTask {
+
+    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
+
+        InputStreamReader reader = new InputStreamReader(inputStream, charset);
+        StringBuilder sb = new StringBuilder();
+        int a;
+        while ((a = reader.read()) != -1) {
+            sb.append((char)a);
+        }
+        reader.close();
+        return sb.toString();
+    }
+}
